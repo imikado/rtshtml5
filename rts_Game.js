d0d3680db54e62d338163a3b8f88e717e7048ec0
imikado <imikado@gmail.com>
2014-01-04 23:22:14 +0100

    maj optimisation + documentation

--- rts_Game.js
+++ rts_Game.js
@@ -1,14 +1,23 @@
 //Game
 function Game(){
+	//tableau de coordonnées des unités
 	this.tCoordUnit=Array();
+	//tableau de corrdonnées des batiments/arbres/mine d'or
 	this.tCoordBuild=Array();
+	//le mode en cours 
 	this.mode='';
+	//l'element selectionné (unité/batiment...)
 	this.selected='';
+	//le batiment selectionné à construire
 	this.buildcreation='';
+	//le tableau des cases visibles sur la carte
 	this.tVisible=Array();
+	//tableau contenant tous les batiments (utilisé pour reconstruire la map lors d'un scroll)
 	this.tBuild=Array();
+	//idem pour les unités
 	this.tUnit=Array();
 	
+	//ressources
 	this.iOr=250;
 	this.iWood=150;
 }
@@ -145,43 +154,52 @@ Game.prototype={
 		}
 		return true;
 	},
+	//appelé lors d'un clic droit sur le canvas (pour un deplacement d'unité)
 	clickdroit:function(e){
+		//recuperation des coordonnées "tableau" x y
 		var x=this.getX(e);
 		var y=this.getY(e);
-		
+
+		//recuperation de batiment sur ces coordonnées
 		var aBuild=this.getBuild(x,y);
-		
-		//or
+
+		//si une unité est sélectionnée et que le batiment cliqué est une mine ou du bois
 		if(this.selected!='' && aBuild && ( aBuild.name=='or' || aBuild.name=='wood' )){
+			//on créé une ronde du bois/or vers le QG
+			//pour alimenter les ressources Or/bois
+			
+			//on indique que la destination de cycle c'est la mine d'or ou l'arbre
 			this.selected.cycleToX=x;
 			this.selected.cycleToY=y;
-			
+
+			//on indique que la provenance du cycle c'est le QG
 			this.selected.cycleFromX=QGx;
 			this.selected.cycleFromY=QGy;
-			
+
+			//on donne comme cible de deplacement la mine d'or/l'arbre cliqué
 			this.selected.setTarget(x,y);
-		}
+		}else if(!this.isWalkable(x,y)){
+			//si la case n'est pas accessible, il ne se passe rien
 		
-		if(!this.isWalkable(x,y)){
 			return false;
-		}
-		
-		if(this.selected!=''){
-		
+		}else if(this.selected!=''){
+			//si la case est accessible, on y indique à l'unité d'y aller
 			this.selected.setTarget(x,y);
-			//this.clearSelect();
-			
-				 
 		}
-		
+
 		return false;
 	},
+	//appelée lors d'un clic gauche sur le canvas (sélection d'une unité/batiment)
 	click:function(e){
+		//recuperation des coordonnées "tableau" x y
 		var x=this.getX(e);
 		var y=this.getY(e);
 		
+		//si l'utilisateur a séléctionné un batiment à construire
 		if(this.buildcreation!=''){
 			
+			//on veririfie ensuite les coordonnées 
+			//des 4 cases nécéssaires à la construction du batiment
 			var ok=1;
 			if(!oGame.checkCoordVisible(x,y) || !oGame.checkCoord(x,y) ){
 				ok=0;
@@ -194,38 +212,48 @@ Game.prototype={
 			}
 			
 			if(!ok){
+				//si une des cases indisponible, on annule
 				return;
 			}
 			
-			 
+			//si c'est ok, on efface la selection d'emplacement
 			this.buildcreation.clear();
+			//on indique à l'unité qui doit construire
+			//le batiment à construire
 			this.selected.buildOn(this.buildcreation);
 			
+			//on annule la construction en cours
 			this.buildcreation='';
 			
 			return;
 			 
 		} 
 		
+		//on recherche si il y a quelquechose aux coordonnées
 		var oUnit=this.getUnit(x,y);
 		var oBuild=this.getBuild(x,y);
 		
-		
+		//si il y a une unité
 		if(oUnit){
+			//on selectionne celle-ci
 			this.select(oUnit);
 		}else if(oBuild){
+			//si  il y a un batiment,
+			//on le selectionne
 			this.select(oBuild);
 		}else{
+			//sinon on supprime la selection sur le canvas
 			console.log('pas trouve');
 			this.clearSelect();
 		}
 	},
 	saveBuild:function(oBuild){
-            
+        //on recupere les coordonnées du batiment 
         var y=parseInt(oBuild.y);
         var x=parseInt(oBuild.x);
             
-		
+		//on enregistre dans un tableau indexé
+		//les nouvelles coordonnées
 		if(!this.tCoordBuild[y]){
 			this.tCoordBuild[y]=Array();
 		}
@@ -236,8 +264,6 @@ Game.prototype={
 		this.tCoordBuild[y+1][x]=oBuild;
 		this.tCoordBuild[y+1][x+1]=oBuild;
 		this.tCoordBuild[y][x+1]=oBuild;
-		
-		//tBuild.push(oBuild);
 		
 	},
 	getBuild:function(x,y){
@@ -258,16 +284,18 @@ Game.prototype={
 		this.setVisibility(x,y);
 	},
 	saveUnit:function(oUnit){
-		
+		//on recupere les coordonnées de l'unité
 		var y=oUnit.y;
 		var x=oUnit.x;
 		
+		//on enregistre dans un tableau indexé
+		//les nouvelles coordonnées
 		if(!this.tCoordUnit[y]){
 			this.tCoordUnit[y]=Array();
 		}
 		this.tCoordUnit[y][x]=oUnit;
-		//console.log('saveUnit '+y+' '+x);
 		
+		//on rend la zone visible
 		this.setVisibility(x,y);
 	},
 	getUnit:function(x,y){
@@ -281,22 +309,27 @@ Game.prototype={
 		this.mode='move';
 	},
 	clearSelect:function(){
+		//on efface le calque
 		oLayer_select.clear();
 		this.selected='';
+		//on efface le bloc du bas
 		this.resetNav();
 	},
 	select:function(oMix){
+		//on enregistre l'unité/batiment
 		this.selected=oMix;
 		
+		//on demande son dessin
 		this.drawSelected();
 		
+		//on affiche la navigation
 		oMix.buildNav();
-		
-		
 	},
 	drawSelected:function(){
+		//on efface le calque
 		oLayer_select.clear();
 		
+		//on dessine un cadre sur un des calques au dimension de l'élement
 		oLayer_select.drawRectStroke((this.selected.x-currentX)*widthCase,(this.selected.y-currentY)*heightCase,this.selected.width,this.selected.height,'#880044',3);
 	},
 	resetNav:function(){
@@ -304,22 +337,24 @@ Game.prototype={
 	},
 	refreshUnit:function(){
 		
-		//oLayer_perso.clear();
-		
+		//on boucle sur les unités existantes
 		for(var i=0;i< this.tUnit.length;i++){
 			var oUnit= this.tUnit[i];
 			
+			//si l'unité doit se rendre quelques part
 			if(oUnit.targetX!='' && oUnit.targetY!='' && (oUnit.targetX!=oUnit.x || oUnit.targetY!=oUnit.y) ){
 			
 				var vitesse=1;
 				var vitesse2=vitesse*-1;
 				
+				//on efface le dessin sur le calques
 				oUnit.clear();
 				
-				
+				//on initialise les nouvelles coordonnées
 				var newX=oUnit.x;
 				var newY=oUnit.y;
 				
+				//on fait evoluer les coordonnées vers la destination
 				if(oUnit.targetX!='' && oUnit.x < oUnit.targetX ){
 					newX+=vitesse;
 				}else if(oUnit.targetX!='' && oUnit.x > oUnit.targetX ){
@@ -331,85 +366,107 @@ Game.prototype={
 					newY-=vitesse;
 				}
 				
+				//on verifie si aux coordonnées cible, il y a un batiment
 				var aBuild=this.getBuild(newX,newY);
 				
-				
+				//si la cible est le QG
 				if(aBuild && aBuild.name=='building'){
 					oUnit.x=newX;
 					oUnit.y=newY;
 					
+					//on definit la nouvelle cible
 					oUnit.setTarget(oUnit.cycleToX,oUnit.cycleToY);
 					
+					//si l'unité transportait de l'or
 					if(oUnit.or >0){
+						//on ajoute une ressource or
 						this.addRessourceOr(oUnit.or);
 					}else 	if(oUnit.wood >0){
+						//idem pour le bois
 						this.addRessourceWood(oUnit.wood);
 					}
 
-					
+					//on reset les ressources de l'unité
 					oUnit.or=0;
 					oUnit.wood=0;
 				
+				//si la cible c'est un arbre et que le compteur est inferieur à N
 				}else if(aBuild && aBuild.name=='wood' && oUnit.counter < 8){
+					//on met en place un compteur 
+					//pour que la ressources mette du temps
+					//a recuperer la ressource
 					oUnit.counter+=1;
 					
 					continue;
-				}else if(aBuild && aBuild.name=='wood' && oUnit.counter >= 8){
 					
+				//si le compteur est superieur à N
+				}else if(aBuild && aBuild.name=='wood' && oUnit.counter >= 8){
+					//on indique à l'unité qu'elle transporte 10
 					oUnit.wood=10;
 					
 					oUnit.x=newX;
 					oUnit.y=newY;
 					
+					//on remet le compteur à 0
 					oUnit.counter=0;
 					
+					//on redéfinit la nouvelle cible (c'est un cycle)
 					oUnit.setTarget(oUnit.cycleFromX,oUnit.cycleFromY);
-					
-					
+				
+				//si la cible c'est une mine d'or et que le compteur est inferieur à N
 				}else if(aBuild && aBuild.name=='or' && oUnit.counter < 8){
+					//on met en place un compteur 
+					//pour que la ressources mette du temps
+					//a recuperer la ressource
 					oUnit.counter+=1;
 					
 					continue;
+				//si le compteur est superieur à N
 				}else if(aBuild && aBuild.name=='or' && oUnit.counter >= 8){
-					
+					//on indique à l'unité qu'elle transporte 10
 					oUnit.or=10;
 					
 					oUnit.x=newX;
 					oUnit.y=newY;
 					
+					//on remet le compteur à 0
 					oUnit.counter=0;
 					
+					//on redéfinit la nouvelle cible (c'est un cycle)
 					oUnit.setTarget(oUnit.cycleFromX,oUnit.cycleFromY);
 					
 				}else if(this.checkCoord(newX,newY)){
+					//si la coordonnées est libre
 					oUnit.x=newX;
 					oUnit.y=newY;
 				}else if(this.checkCoord(oUnit.x,newY)){
-					//oUnit.x=newX;
+					//si la coordonnées est libre (cas d'un evitement d'obstacle)
 					oUnit.y=newY;
 				}else if(this.checkCoord(newX,oUnit.y)){
+					//si la coordonnées est libre (cas d'un evitement d'obstacle)
 					oUnit.x=newX;
-					//oUnit.y=newY;
 				}else if(this.checkCoord(newX,oUnit.y)){
+					//si la coordonnées est libre (cas d'un evitement d'obstacle)
 					oUnit.x=newX;
-					//oUnit.y=newY;
 				}else if(this.checkCoord(oUnit.x+vitesse2,oUnit.y)){
-					//oUnit.x=newX;
+					//si la coordonnées est libre (cas d'un evitement d'obstacle)
 					oUnit.x=oUnit.x+vitesse2;
 				}else if(this.checkCoord(oUnit.x,oUnit.y+vitesse2)){
-					//oUnit.x=newX;
+					//si la coordonnées est libre (cas d'un evitement d'obstacle)
 					oUnit.y=oUnit.y+vitesse2;
 				}
 				
-				
+				//on dessine l'unité
 				oUnit.build();
 				
 				console.log('recalcul');
+				//on met à jour la partie visible de la carte
 				oGame.displayVisibility();
 			}
 		
 		}
 		
+		//on redessine le cadre de selection
 		this.drawSelected();
 		
 	},
@@ -423,21 +480,35 @@ Game.prototype={
 	mousemove:function(e){
 		sDirection='';
 		if(this.buildcreation){
+			//si une construction est en cours
 		
+			//on recupere les coordonnées
 			var x=this.getX(e);
 			var y=this.getY(e);
 			
+			//on efface la derniere position de la construction théorique
 			this.buildcreation.clear();
+			//on deplace le plan aux nouvelles coordonnées
 			this.buildcreation.x=x;
 			this.buildcreation.y=y;
+			//on affiche le plan
 			this.buildcreation.build();
 		}else if(this.getYmouse(e) < 5){
+			//si les coordonnées y sont inferieur à 5: 
+			//scroll de la map vers le haut
 			sDirection='up';
 		}else if(this.getYmouse(e) > (oLayer_map.height-10) && this.getYmouse(e) < oLayer_map.height-1 && (this.getXmouse(e) < 100 || this.getXmouse(e) > 500)){
+			//ici on verifie plusieurs choses pour laisser un passage vers le bas
+			//vous pouvez voir ces zones de selection en orange
+			//si c'est le cas, scroll de la map vers le bas
 			sDirection='down';
 		}else if(this.getXmouse(e) < 5){
+			//si les coordonnées x sont inferieur à 5: 
+			//scroll de la map vers la gauche
 			sDirection='left';
 		}else if(this.getXmouse(e) > (oLayer_map.width-10)){
+			//si les coordonnées x sont superieur à la largeur de la map -10: 
+			//scroll de la map vers la droite
 			sDirection='right';
 		}
 		
